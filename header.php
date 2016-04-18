0a1d1e5363d64e5ebf036621ddaad1dc35b720e3
Padhraic Coogan <pcoogan@gmail.com>
2016-04-19 00:23:13 +0900

    add test menu

--- header.php
+++ header.php
@@ -27,34 +27,12 @@
 
 <body <?php body_class(); ?>>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only"><?php esc_html_e( 'Toggle Navigation', 'cleanblog' ); ?></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-				<?php if ( is_front_page() && is_home() ) : ?>
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="navbar-brand"><?php bloginfo( 'name' ); ?></a>
-				<?php else : ?>
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="navbar-brand"><?php bloginfo( 'name' ); ?></a>
-				<?php endif; ?>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav navbar-right">
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'items_wrap' => '%3$s' ) ); ?>
-				</ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
+     <nav id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <?php wp_nav_menu( array( 'theme_location' => 'primary', 'items_wrap' => '%3$s' ) ); ?>
+        </ul>
     </nav>
+    
 	
 	<?php cleanblog_header(); ?>
 
